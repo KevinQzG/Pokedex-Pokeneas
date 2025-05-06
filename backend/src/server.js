1aedc2c2961308e09c8270c144a1ef3447917144
Kevin Quiroz <kquirozg@eafit.edu.co>
2025-05-06 14:02:43 -0500

    feat: implement Pokenea JSON and image routes with container ID

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const routes = require('./routes/index');
+const PORT = process.env.PORT || 5000;
+app.use('/', routes);
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
